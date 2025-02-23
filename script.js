ca53e704ad99241b00de930e3aa0648262174d01
Tanmaysingh2210 <tanmayrajpoot2210@gmail.com>
2025-02-23 20:17:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -55,62 +55,131 @@ getNext();
 
 
 //To add answer divs for FREQUENTLY ASKED QUESTIONS
-let faq1 = document.querySelector(".info").querySelector(".faqs").querySelector(".faq-1");
-console.log(faq1);
-faq1.addEventListener("click", function () {
-    faq1.getElementsByTagName("img").transform= ;
-
-    
-})
-
-let faq2 = document.querySelector(".info").querySelector(".faqs").querySelector(".faq-2");
-faq2.addEventListener("click", function () {
-    faq2.getElementsByTagName("img").transform= ;
-
-    
-
-})
-
-let faq3 = document.querySelector(".info").querySelector(".faqs").querySelector(".faq-3");
-faq3.addEventListener("click", function () {
-    faq3.getElementsByTagName("img").transform= ;
-
-    
+let faq = document.querySelector(".info").querySelector(".faqs").getElementsByClassName("faq");
+console.log(faq);
 
 
-})
+// document.getElementById("faqs").addEventListener("click", function(event) {
+//     if (event.target.classList.contains("faq")) {
+//         handleClick(event.target);
+//     }
+// });
 
-let faq4 = document.querySelector(".info").querySelector(".faqs").querySelector(".faq-4");
-faq4.addEventListener("click", function () {
-    faq4.getElementsByTagName("img").transform= ;
+// function handleClick(div) {
+//     console.log("Div clicked:", div);
+//     div.lastElementChild.style.display = "block"
+// }
 
-    
-})
+faq.forEach(e => {
+    e.addEventListener("click", function(){
+        e.lastElementChild.style.display = "block";
+        let img = e.getElementsByTagName("img")[0]; 
+        if (img) {
+            img.style.transform = "rotate(45deg)"; 
+        }
 
-let faq5 = document.querySelector(".info").querySelector(".faqs").querySelector(".faq-5");
-faq5.addEventListener("click", function () {
-    faq5.getElementsByTagName("img").transform= ;
 
-    let div5 = document.createElement("div");
-    div5.classList.add("expanded", "expanded-5");
-    div5.innerHTML = `<p>
-                Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.
-            </p>`
-    div5.after(faq5);
-})
-
-let faq6 = document.querySelector(".info").querySelector(".faqs").querySelector(".faq-6");
-faq6.addEventListener("click", function () {
-    faq6.getElementsByTagName("img").transform= ;
-
-    let div6 = document.createElement("div");
-    div6.classList.add("expanded", "expanded-6");
-    div6.innerHTML = `<p>
-                The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.
-            </p>
-            <br>
-            <p>Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.</p>`
-    div6.after(faq6);
+    })
 });
+
+
+
+
+
+// faq.addEventListener("click", function ( ) {
+//     faq.lastElementChild.style.display = "block";
+// })
+
+
+
+// async function getAns(){
+
+//     let faq1 = document.querySelector(".info").querySelector(".faqs").querySelector(".faq-1");
+//     faq1.addEventListener("click", function () {
+//         faq1.lastElementChild.style.display = "block";
+//         console.log(faq1.lastElementChild);
+    
+//         let img = faq1.getElementsByTagName("img")[0]; // Select first <img> inside .faq-1
+//         if (img) { // Check if image exists
+//             img.style.transform = "rotate(45deg)"; // Correct way to rotate image
+//         }
+        
+
+
+//         faq1.addEventListener("click",function(){
+//             this.style.cssText = "";
+//         })
+    
+    
+//     })
+// }
+// getAns();
+
+
+
+// let faq2 = document.querySelector(".info").querySelector(".faqs").querySelector(".faq-2");
+// faq2.addEventListener("click", function () {
+//     faq2.lastElementChild.style.display = "block";
+//     console.log(faq2.lastElementChild);
+
+//     let img = faq2.getElementsByTagName("img")[0]; // Select first <img> inside .faq-1
+//     if (img) { // Check if image exists
+//         img.style.transform = "rotate(45deg)"; // Correct way to rotate image
+//     }
+
+
+// })
+
+// let faq3 = document.querySelector(".info").querySelector(".faqs").querySelector(".faq-3");
+// faq3.addEventListener("click", function () {
+//     faq3.lastElementChild.style.display = "block";
+//     console.log(faq3.lastElementChild);
+
+//     let img = faq3.getElementsByTagName("img")[0]; // Select first <img> inside .faq-1
+//     if (img) { // Check if image exists
+//         img.style.transform = "rotate(45deg)"; // Correct way to rotate image
+//     }
+    
+    
+// })
+// // faq3.addEventListener("click", function () {
+// //     this.style.cssText = "";
+// // })
+
+// let faq4 = document.querySelector(".info").querySelector(".faqs").querySelector(".faq-4");
+// faq4.addEventListener("click", function () {
+//     faq4.lastElementChild.style.display = "block";
+//     console.log(faq4.lastElementChild);
+
+//     let img = faq4.getElementsByTagName("img")[0]; // Select first <img> inside .faq-1
+//     if (img) { // Check if image exists
+//         img.style.transform = "rotate(45deg)"; // Correct way to rotate image
+//     }
+
+// })
+
+// let faq5 = document.querySelector(".info").querySelector(".faqs").querySelector(".faq-5");
+// faq5.addEventListener("click", function () {
+
+//     faq5.lastElementChild.style.display = "block";
+//     console.log(faq5.lastElementChild);
+
+//     let img = faq5.getElementsByTagName("img")[0]; // Select first <img> inside .faq-1
+//     if (img) { // Check if image exists
+//         img.style.transform = "rotate(45deg)"; // Correct way to rotate image
+//     }
+// })
+
+// let faq6 = document.querySelector(".info").querySelector(".faqs").querySelector(".faq-6");
+// faq6.addEventListener("click", function () {
+//     faq6.lastElementChild.style.display = "block";
+//     console.log(faq6.lastElementChild);
+
+//     let img = faq6.getElementsByTagName("img")[0]; // Select first <img> inside .faq-1
+//     if (img) { // Check if image exists
+//         img.style.transform = "rotate(45deg)"; // Correct way to rotate image
+//     }
+
+// });
 
 
